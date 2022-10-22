8f3dd2f0bda9f40bcb1840bce0d8f0ec5169a174
SUWAN <106606698+kaYeonTan@users.noreply.github.com>
2022-10-23 00:04:12 +0900

    1023_getline

---
+++ 1021_String/1021_String/GetLine.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch1, ch2;
+    ch1 = cin.get();
+    ch2 = cin.get();
+    cout << ch1 << "  " <<ch2 << endl;
+
+    return 0;
+}
