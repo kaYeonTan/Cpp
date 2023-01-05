262ecc35d14f422fd4ada9f2442c38b883e10325
SUWAN <106606698+kaYeonTan@users.noreply.github.com>
2023-01-05 10:57:57 +0900

    List2

--- List/List/List.cpp
+++ List/List/List.cpp
@@ -5,9 +5,13 @@ using namespace std;
 int main()
 {
 	int n = 5;
-	list<int>  intList(n, 2);
+	list<int>  list_original{ 2,3,4,5 };
+	list<int> list_new(list_original);			//복사생성자를 이용해 복사
+	list<int> list_opercopy = list_new;	//대입연산자를 통해 복사
 
-	for (int i : intList)		printf("%d, ", i);
+	list<int> list_itercopy(list_opercopy.begin(), list_opercopy.end()); //반복자 사용해서 복사
+
+	for (int i : list_itercopy)		printf("%d ", i);
 
 	return 0;
 }
