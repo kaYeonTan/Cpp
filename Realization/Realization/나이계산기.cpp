97cc7d49cc9868ed6514310f5025eb6ed89d0210
SUWAN <106606698+kaYeonTan@users.noreply.github.com>
2023-02-19 10:54:57 +0900

    나이계산기

--- Realization/Realization/나이계산기.cpp
+++ Realization/Realization/나이계산기.cpp
@@ -2,13 +2,17 @@
 #include<vector>
 #include<string>
 #include<Windows.h>
+#include<stdbool.h>
 using namespace std;
 
+int curYear = -1, curMon = -1, curDay = -1;
+int targetBirthYear = -1, targetBirthMon = -1, targetBirthDay = -1;
+//  한국식 나이   만나이
+int korAge = -1, age = -1;
 void main()
 {
 	cout << "오늘 날짜를 입력해주세요(공백을 기준으로 연, 월, 일을 입력)" << endl;
 	//연월일 입력받기
-	int curYear = -1, curMon = -1, curDay = -1;
 	cin >> curYear >> curMon >> curDay;
 	//정상적인 입력을 받았을 경우
 	//잘못 입력받았을 경우
@@ -26,11 +30,45 @@ void main()
 	cout <<"오늘은 " << curYear << "년 " << curMon << "월 " << curDay<<"일 입니다." << endl;
 
 	//신상 정보 입력
-	string name = "";
+	string targetName = "";
 	cout << "대상의 이름을 입력해주세요" << endl;
-	cin >> name;
-	cout << "대상의 생년월일과 주민번호 앞자리를 입력해주세요(예 : [][][][][][]-[]XXXXXX" << endl;
-	string registrationNum = "";
-	cin >> registrationNum;
+	cin >> targetName;
+		
+	cout << "대상의 생년월일을 입력해주세요(공백을 기준으로 입력합니다)" << endl;
+	cin >> targetBirthYear >> targetBirthMon >> targetBirthDay;
+
+	while (targetBirthMon > 12 || 1 > targetBirthMon || targetBirthDay > 31 || 1 > targetBirthDay)
+	{
+		if (targetBirthMon > 12 || 1 > targetBirthMon) {
+			cout << "월을 잘 못 입력하였습니다. 재입력해주세요." << endl;
+			cin >> targetBirthMon;
+		}
+		if (targetBirthDay > 31 || 1 > targetBirthDay) {
+			cout << "날짜를 잘 못 입력하였습니다. 재입력해주세요." << endl;
+			cin >> targetBirthDay;
+		}
+	}
+	cout <<targetName<<"의 생년월일은 " << targetBirthYear << "년 " << targetBirthMon << "월 " << targetBirthDay <<"일 입니다." << endl;
+	
+	//한국식 나이
+	korAge = (curYear - targetBirthYear)+1;
+	int temp = korAge;
+	string birthDay ="";
+	//만나이
+	if (curMon < targetBirthMon) age = temp - 2;		//생일 안지남
+	else if (curMon > targetBirthMon) age = --temp;	//생일 지남
+	else if (curMon == targetBirthMon)					//달이 같음
+	{
+		if (curDay < targetBirthDay) age = temp - 2;		//생일 안지남
+		else if (curDay > targetBirthDay) age = --temp;	//생일 지남
+		else if (curDay == targetBirthDay)					//생일
+		{
+			age = --temp;
+			birthDay = targetName + "님 생일 축하드립니다!";
+		}
+	}
+
+	cout << targetName << "님의 나이는" << korAge << "세 (만 " << age << "세) 입니다.\n" << birthDay << endl;
+
 
 }
