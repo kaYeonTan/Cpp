26ce803aadf87f32bce972bdf6f85212d7adfffb
SUWAN <106606698+kaYeonTan@users.noreply.github.com>
2023-03-22 21:12:32 +0900

    hash

---
+++ Hash/Hash/hash.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_TABLE 8191
+#define MAX_KEY   64
+
+typedef long long ll;
+
+typedef struct Hash
+{
+	char key[MAX_KEY + 1];
+	int data;
+} Hash;
+
+int main()
+{
+	 
+}
